b1972450ddd4c6100d57cfcc6ef2aba8f87e5182
hymndrazill <srihafiras@yahoo.fr>
2022-10-14 18:09:29 +0100

    adding the Scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link,
+  Route,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import './style.scss'
+
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/post/:id/',
+        element: <Single/>
+      },
+      {
+        path:'/write',
+        element: <Write/>
+      },
+     
+    ]
+  },
+  {
+    path:'/login',
+    element: <Login/>
+  },
+  {
+    path:'/register',
+    element: <Register/>
+  },
+ 
+]); 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+        <RouterProvider router={router}/>
+        </div>
     </div>
   );
 }
+
+
 
 export default App;
