03c6d9b01235bf02c124a7a6b9e2de385f728ac5
hymndrazill <srihafiras@yahoo.fr>
2022-10-14 18:08:24 +0100

    added pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import logo from '../img/logo.png'
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className='link' to='/?cat=art'>
+            <h6>Art</h6>
+          </Link>
+          <Link className='link' to='/?cat=science'>
+            <h6>Science</h6>
+          </Link>
+          <Link className='link' to='/?cat=technology'>
+            <h6>Technology</h6>
+          </Link>
+          <Link className='link' to='/?cat=cinema'>
+            <h6>Cinema</h6>
+          </Link>
+          <Link className='link' to='/?cat=desgin'>
+            <h6>Desgin</h6>
+          </Link>
+          <Link className='link' to='/?cat=food'>
+            <h6>food</h6>
+          </Link>
+          <span>John</span>
+          <span>Log Out</span>
+          <span className='write'>
+            <Link className='link' to='/write'>Write</Link>
+          </span>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
