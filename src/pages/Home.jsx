6cdde6a49ae55822e2d1cb668b60ec1f818b4bba
hymndrazill <srihafiras@yahoo.fr>
2022-10-20 02:09:06 +0100

    adding context to handle users & their username

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,54 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 import {Link} from 'react-router-dom'
+import axios from "axios";
 const Home = () => {
+const [posts,setPosts] = useState([]);
 
-const posts = [
-  {
-    id:1,
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-    img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-  },
-  {
-    id:2,
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-    img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-  },
-  {
-    id:3,
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-    img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-  },
-  {
-    id:4,
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-    img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-  },
-  {
-    id:5,
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-    img:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-  },
-]
+useEffect(()=>{
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("/posts")
+      setPosts(res.data);
+    } catch (err){
+      console.log(err);
+    }
+  }
+  fetchData();
+})
+// const posts = [
+//   {
+//     id:1,
+//     title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+//     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+//     img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+//   },
+//   {
+//     id:2,
+//     title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+//     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+//     img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+//   },
+//   {
+//     id:3,
+//     title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+//     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+//     img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+//   },
+//   {
+//     id:4,
+//     title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+//     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+//     img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+//   },
+//   {
+//     id:5,
+//     title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+//     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+//     img:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+//   },
+// ]
 
 
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../context/authContext';
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [inputs,setInputs] = useState({
+    username:"",
+    password:"",
+  })
+  const [error, setError] = useState(null);
+
+  const {login} = useContext(AuthContext);
+  
+  const handleChange = e =>{
+    setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
+}
+
+const handleSubmit= async e =>{
+  e.preventDefault();
+  try {
+   await login(inputs);
+    navigate("/")
+  } catch (err){
+    console.log("problem here");
+    setError(err.response.data)
+  }
+
+}
   return (
     <div className='auth'>
       <h1> Login</h1>
       <form>
-        <input required type='text' placeholder='Username'/>
-        <input required type='password' placeholder='Password'/>
-        <button type='submit'>Login</button>
-        <p>This is an error!!</p>
+        <input onChange={handleChange} required type='text' name="username" placeholder='Username'/>
+        <input onChange={handleChange} required type='password' name="password" placeholder='Password'/>
+        <button onClick={handleSubmit} type='submit'>Login</button>
+      {error && <p>{error}</p>}  
         <span> Don't have an account?<Link to="/register">Register</Link></span>
 
       </form>
