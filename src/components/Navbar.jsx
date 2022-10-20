9f5a9107dc6644f297b9490d428cd8e49ed5f2fd
hymndrazill <srihafiras@yahoo.fr>
2022-10-20 02:09:50 +0100

    added a home link for the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import logo from '../img/logo.png'
 import {Link} from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 const Navbar = () => {
+  const {currentUser,logout} = useContext(AuthContext);
   return (
     <div className='navbar'>
       <div className="container">
         <div className="logo">
+          <Link to="/">
           <img src={logo} alt="" />
+          </Link>
         </div>
         <div className="links">
           <Link className='link' to='/?cat=art'>
@@ -27,8 +32,10 @@ const Navbar = () => {
           <Link className='link' to='/?cat=food'>
             <h6>food</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+        { currentUser ?  <span onClick={logout}>Logout</span>
+        : <Link className="link" to="/login">login</Link>  
+      }
           <span className='write'>
             <Link className='link' to='/write'>Write</Link>
           </span>
