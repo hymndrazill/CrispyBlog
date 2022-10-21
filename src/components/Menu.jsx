11e12629e3a3c679dff1291f235afdf44aa39a42
hymndrazill <srihafiras@yahoo.fr>
2022-10-21 16:58:28 +0100

    fetching data from sql instead of dummy ones

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const Menu = () => {
-    const posts = [
-        {
-          id:1,
-          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-          desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-          img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-        },
-        {
-          id:2,
-          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-          desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-          img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-        },
-        {
-          id:3,
-          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-          desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-          img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-        },
-        {
-          id:4,
-          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-          desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-          img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-        },
-        {
-          id:5,
-          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
-          desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
-          img:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-        },
-      ]
+const Menu = ({cat}) => {
+  
+const [posts,setPosts] = useState([]);
+
+useEffect(()=>{
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`/posts/?cat=${cat}`)
+      setPosts(res.data);
+    } catch (err){
+      console.log(err);
+    }
+  }
+  fetchData();
+}, [cat])
+    // const posts = [
+    //     {
+    //       id:1,
+    //       title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+    //       desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+    //       img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+    //     },
+    //     {
+    //       id:2,
+    //       title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+    //       desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+    //       img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+    //     },
+    //     {
+    //       id:3,
+    //       title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+    //       desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+    //       img:'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+    //     },
+    //     {
+    //       id:4,
+    //       title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+    //       desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+    //       img:'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+    //     },
+    //     {
+    //       id:5,
+    //       title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus non fugit nemo iure earum, mollitia et reprehenderit hic cumque maxime! Molestias recusandae cupiditate quo repudiandae magnam! Commodi, rem tenetur.',
+    //       desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.',
+    //       img:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+    //     },
+    //   ]
   return (
     <div className='menu'>
         <h1>Other Posts you may like.</h1>
