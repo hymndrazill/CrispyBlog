5f3a4bd5a2711c19be77a0ea646478b13536e856
hymndrazill <srihafiras@yahoo.fr>
2022-10-21 17:00:15 +0100

    added the DOMParser for security issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,6 +50,10 @@ useEffect(()=>{
 //     img:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
 //   },
 // ]
+const getText = html =>{
+  const doc = new DOMParser().parseFromString(html,'text/html');
+  return doc.body.textContent
+}
 
 
   return (
@@ -58,14 +62,14 @@ useEffect(()=>{
         {posts.map( post =>(
           <div className="post" key={post.id}>
             <div className="img">
-              <img src={post.img} alt="" />
+              <img src={`../upload/${post.img}`} alt="" />
             </div>
           <div className="content">
             <Link className='link' to={`/post/${post.id}`}>
               <h1>{post.title}</h1>
             </Link>
 
-              <p>{post.desc}</p>
+              <p>{getText(post.desc)}</p>
               <button>Read More.</button>
           </div>
 
