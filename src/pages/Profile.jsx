934fbbfe0f8299250c35265fae0f75f9c973b00a
hymndrazill <srihafiras@yahoo.fr>
2022-10-23 23:49:33 +0100

    adding state and behavior in the page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,27 +1,58 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/authContext'
 
 const Profile = () => {
+  const {currentUser} = useContext(AuthContext)
+  let userId= currentUser.id
+  const [userInfo,setUserInfo] = useState({
+    username:'',
+    email:'',
+    password:'',
+    img:''
+  })
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setUserInfo(prev=> ({...prev, [e.target.name]: e.target.value}))
+  }
+  const updateUser =  async (e)=>{
+    e.preventDefault();
+    try {
+      const res = await axios.put(`/users/${userId}`, userInfo);
+      console.log(res?.data);
+      navigate("/")
+  
+    } catch(err) {
+      console.log('error');
+      console.log(err);
+    }
+  }
+
+
   return (
     <div className='profile'>
-        <h1>Edit your portfolio</h1>
-    <div className="user_info">
-    <label htmlFor="">Username</label>
-      <input text="text" placeholder='Username '/>
-    </div>
-    <div className="user_info">
-      <label htmlFor="">email</label>
-      <input text="email" placeholder='Email '/>
+        <h1>You can edit your infos here: {currentUser.username}</h1>
+      <div className="user_profile">
+      <div className='user_img'>
+        <img className="userInfoImg" src={currentUser.img} alt="" />
       </div>
       <div className="user_info">
-      <labe<div className="user_info">l htmlFor="">password</label>
-      <input text="password" placeholder='password '/>
+      <input onChange={handleChange} name="username" text="text" placeholder='Username '/>
       </div>
-      <div className="user_info">
-      <label htmlFor="">image</label>
-      <input text="file" placeholder='Your new image. '/>
-</div>
 
-
+        <div className="user_info">
+        <input onChange={handleChange} name="email" text="email" placeholder='Email '/>
+        </div>
+        <div className="user_info">
+        <input onChange={handleChange} name="password" text="password" placeholder='password '/>
+        </div>
+        <div className="user_info">
+          <input onChange={handleChange} name="img" type="file"/>
+        </div>
+        <button onClick={updateUser}className='profile_btn'>Edit User.</button>
+        </div>
     </div>
   )
 }
